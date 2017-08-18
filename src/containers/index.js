28e9d46b1081fb75917195b3fec0eb51d8dcf2ce
jinye <1037393965@qq.com>
2017-08-18 20:10:29 +0800

    jinye

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
 import PureRenderMixin from "react-addons-pure-render-mixin";
-
+import LocalStore from '../util/localStorage';
+import {CITYNAME} from '../config/localstoreKey';
 
 export default class App extends Component {
     constructor() {
         super();
-        this.shouldComponentUpdate=PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            initDone: false
+        }
+    }
+
+    componentDidMount() {
+        let cityName = LocalStore.getItem(CITYNAME);
+        if (cityName == null) {
+            cityName = "北京"
+        }
+
+        this.setState({
+            initDone: true
+        })
+
     }
 
     render() {
         return (
             <div className="App">
-                <div>Header</div>
-                {this.props.children}
-                <div>Footer</div>
+                {
+                    this.state.initDone
+                        ? this.props.children
+                        : <div>加载中。。。</div>
+                }
             </div>
         );
     }
