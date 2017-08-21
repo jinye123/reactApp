3f12bb1a75f905f62c0dbc90b857dc7d76b16faf
jinye <1037393965@qq.com>
2017-08-21 14:23:21 +0800

    first

--- src/containers/Home/subpage/List.js
+++ src/containers/Home/subpage/List.js
@@ -4,7 +4,8 @@ import {connect} from 'react-redux';
 import {FETCH_STARTED_HOME_LIST, FETCH_SUCCESS_HOME_LIST, FETCH_FAILURE_HOME_LIST} from '../../../actionTypes/HomeList';
 import {fetchGetData} from '../../../fetch/actions/fetchActions';
 import {fetchStarted, fetchSuccess, fetchFailure} from '../../../actions/HomeList';
-import ListCompoent from '../../../components/List'
+import ListCompoent from '../../../components/List';
+import LoadMore from '../../../components/LoadMore';
 
 import './style.scss';
 
@@ -15,11 +16,21 @@ class List extends Component {
     constructor() {
         super();
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+        this.state = {
+            page: 1
+        }
     }
 
     componentDidMount() {
         this.getList(this.props.userInfo, 0)
     }
+
+    getMoreList = () => {
+        this.getList(this.props.userInfo, this.state.page);
+        this.setState({
+            page: this.state.page + 1
+        })
+    };
 
     getList = (cityName, pageNum) => {
         let MORE_URL = this.setUrl(cityName, pageNum);
@@ -32,39 +43,25 @@ class List extends Component {
 
     render() {
         let status = this.props.listStatus;
-        switch (status) {
-            case FETCH_STARTED_HOME_LIST: {
-                return (
-                    <div>
-                        <h2 className="home-list-title">猜你喜欢</h2>
-                        <h3>{status}</h3>
-                    </div>
-                )
-            }
-            case FETCH_SUCCESS_HOME_LIST: {
-                return (
-                    <div>
-                        <h2 className="home-list-title">猜你喜欢</h2>
-                        <ListCompoent data={this.props.data}/>
-                        {
-
-                        }
-                    </div>
-                )
-            }
-            case FETCH_FAILURE_HOME_LIST: {
-                return (
-                    <div>
-                        <h2 className="home-list-title">猜你喜欢</h2>
-                        <h3>{status}</h3>
-                    </div>
-                )
-            }
-            default: {
-                throw new Error('fetch fail')
-            }
-        }
-
+        return (
+            <div>
+                <h2 className="home-list-title">猜你喜欢</h2>
+                {
+                    status === FETCH_STARTED_HOME_LIST && !this.props.data
+                        ?<div>{FETCH_STARTED_HOME_LIST}</div>
+                        :status === FETCH_STARTED_HOME_LIST && this.props.data
+                        ? <ListCompoent data={this.props.data}/>
+                        : status === FETCH_SUCCESS_HOME_LIST
+                        ? <ListCompoent data={this.props.data}/>
+                        : <div>{FETCH_FAILURE_HOME_LIST}</div>
+                }
+                {
+                    this.props.hasMore
+                        ? <LoadMore listStatus={status} loadMoreFn={this.getMoreList}/>
+                        : ''
+                }
+            </div>
+        )
     }
 }
 
